67b515f896924a343c63e5a834da47372c4d90e9
Ajit Yadav <yadavajita@gmail.com>
2020-03-20 06:56:06 +0000

    First commit of Tic tac toe in git

---
+++ Tic-Tac-Toe_python/py_tutorial.py
@@ -0,0 +1,33 @@
+
+def game_board():
+	print("   0  1  2")
+	for count,row in enumerate(p):
+		print(count,row)
+
+p=[[0,0,1],[0,1,1],[1,0,1]]
+game_board()
+for col in range(len(p)):
+	check=[]
+	for row in p:
+		check.append(row[col])
+	if check.count(check[0]) == len(check) and check[0] != 0:
+		print(f"Player {check[0]} Verticle winner")
+
+for row in p:
+	if row.count(row[0]) == len(row) and row[0] != 0:
+		print(f"Player {row[0]} Horizontal winner")
+
+diag = []
+for i in range(len(p)):
+	diag.append(p[i][i])
+if diag.count(diag[0]) == len(diag) and diag[0] != 0:
+	print(f"Player {diag[0]} Diagonal winner \\")
+
+diag = []
+for idx ,rev_idx in enumerate(reversed(range(len(p)))):
+	diag.append(p[idx][rev_idx])
+if diag.count(diag[0]) == len(diag) and diag[0] != 0:
+	print(f"Player {diag[0]} Diagonal winner /")
+
+
+
